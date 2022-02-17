bf76cb8a6809d971fc349287e16b922f910b744a
Rodrigo Ahumada <github@rholo.cl>
2022-02-17 13:52:02 -0300

    feature(api): update projects file

---
+++ models/projects.ts
@@ -0,0 +1,72 @@
+interface IProject {
+  name: string,
+  shortDescription: string,
+  description: string,
+  technologies: Array<string>
+}
+type IProjects = Array<IProject>
+
+const projects: IProjects = [
+  {
+    name: 'Adere.so',
+    shortDescription: 'Tech lead Front-end...',
+    description: '',
+    technologies: ['AngularJS', 'Vue', 'Vuex', 'AWS', 'Angular']
+  },
+  {
+    name: 'Mall Plaza',
+    shortDescription: 'As UI Developer working with React developing A/B Testing, new features...',
+    description: '',
+    technologies: ['React', 'PWA', 'Angular']
+  },
+  {
+    name: 'Cardif',
+    shortDescription: '',
+    description: '',
+    technologies: ['React', 'JavaScript']
+  },
+  {
+    name: 'Enel',
+    shortDescription: 'UI Designer and developer creating templates for custom CMS develop with react',
+    description: '',
+    technologies: ['React']
+  },
+
+  {
+    name: 'BBVA',
+    shortDescription: '',
+    description: '',
+    technologies: ['Angular', 'AngularJS']
+  },
+  {
+    name: 'Seguros Falabella',
+    shortDescription: '',
+    description: '',
+    technologies: []
+  },
+  {
+    name: 'AGCE',
+    shortDescription: '',
+    description: '',
+    technologies: ['JavaScript', 'JQuery']
+  },
+  {
+    name: 'Factoring Security',
+    shortDescription: 'Design and Create UI Kit template for custom CMS develop with angularJS',
+    description: '',
+    technologies: ['AngularJS', 'HTML', 'CSS']
+  },
+  {
+    name: 'Servicio Nacional del Patrimonio Cultural',
+    shortDescription: 'Develop responsive UI for main website...',
+    description: '',
+    technologies: ['JavaScript', 'HTML', 'CSS']
+  },
+  {
+    name: 'Maui and Sons',
+    shortDescription: 'Develop Landing page for new summer season',
+    description: '',
+    technologies: ['Wordpress', 'HTML', 'CSS']
+  }
+]
+export default projects
