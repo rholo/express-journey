c7d683cc847029bb943630412482e7296232438a
Rodrigo Ahumada <github@rholo.cl>
2022-02-18 11:42:28 -0300

    style: remove comment lines

--- index.ts
+++ index.ts
@@ -1,11 +1,9 @@
-// run `node index.js` in the terminal
 import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3001;
 
 import profile from './models/profile'
-// const projects = require('./models/projects')
 
 app.listen(port, () => {
   // console.log('running server');
@@ -15,7 +13,7 @@ app.get('/', (request: Request, response: Response) => {
   try {
     response.status(200).json({
       description: 'welcome to my node API portfolio',
-      version: 1.0,
+      version: '0.0.4',
       path: 'API',
       availableRoutes: [
         '/api/portfolio'
@@ -35,4 +33,3 @@ app.get('/api/portfolio', (request: Request, response: Response) => {
 });
 
 export default app;
-// console.log(`Hello Node.js v${process.versions.node}!`);
