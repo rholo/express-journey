410041355ef5a466ab2e5ecc240ccdea144fe48f
Rodrigo Ahumada <github@rholo.cl>
2022-02-17 22:34:03 -0300

    feat(api): update profile

--- api/models/profile.js
+++ api/models/profile.js
@@ -9,10 +9,10 @@ const profile = {
     repos: [
         { title: 'GitHub', url: '//github.com/rholo' },
         { title: 'GitLab', url: 'https://gitlab.com/rhockr' },
-        { title: 'NPM', url: 'https://www.npmjs.com/~rholo' }
     ],
     links: [
         { title: 'LinkedIn', url: '//linkedin.com/in/rholo/' },
+        { title: 'NPM', url: 'https://www.npmjs.com/~rholo' },
         { title: 'Gist', url: 'https://gist.github.com/rholo' },
         { title: 'Codepen', url: 'https://codepen.io/rholo' },
         { title: 'Dev.to', url: 'https://dev.to/rholo' }
--- api/models/projects.js
+++ api/models/projects.js
@@ -3,62 +3,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const projects = [
     {
         name: 'Adere.so',
-        shortDescription: 'Tech lead Front-end...',
-        description: '',
+        description: 'Tech lead Front-end...',
         technologies: ['AngularJS', 'Vue', 'Vuex', 'AWS', 'Angular']
     },
     {
         name: 'Mall Plaza',
-        shortDescription: 'As UI Developer working with React developing A/B Testing, new features...',
-        description: '',
+        description: 'As UI Developer working with React developing A/B Testing, new features...',
         technologies: ['React', 'PWA', 'Angular']
     },
     {
         name: 'Cardif',
-        shortDescription: '',
         description: '',
         technologies: ['React', 'JavaScript']
     },
     {
         name: 'Enel',
-        shortDescription: 'UI Designer and developer creating templates for custom CMS develop with react',
-        description: '',
+        description: 'UI Designer and developer creating templates for custom CMS develop with react',
         technologies: ['React']
     },
     {
         name: 'BBVA',
-        shortDescription: '',
         description: '',
         technologies: ['Angular', 'AngularJS']
     },
     {
         name: 'Seguros Falabella',
-        shortDescription: '',
         description: '',
         technologies: []
     },
     {
         name: 'AGCE',
-        shortDescription: '',
         description: '',
         technologies: ['JavaScript', 'JQuery']
     },
     {
         name: 'Factoring Security',
-        shortDescription: 'Design and Create UI Kit template for custom CMS develop with angularJS',
-        description: '',
+        description: 'Design and Create UI Kit template for custom CMS develop with angularJS',
         technologies: ['AngularJS', 'HTML', 'CSS']
     },
     {
         name: 'Servicio Nacional del Patrimonio Cultural',
-        shortDescription: 'Develop responsive UI for main website...',
-        description: '',
+        description: 'Develop responsive UI for main website...',
         technologies: ['JavaScript', 'HTML', 'CSS']
     },
     {
         name: 'Maui and Sons',
-        shortDescription: 'Develop Landing page for new summer season',
-        description: '',
+        description: 'Develop Landing page for new summer season',
         technologies: ['Wordpress', 'HTML', 'CSS']
     }
 ];
